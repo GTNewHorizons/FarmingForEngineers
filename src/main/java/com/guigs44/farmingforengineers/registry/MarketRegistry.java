6d62211a4548c84bd58a1e69b0e6d1787ebed8c6
Clark Fischer <clark.fischer@gmail.com>
2023-10-17 14:57:30 -0700

    Fix registry identifiers
    The registry expects to use `namespace:location`-style identifiers, but
    it was using `#getUnlocalizedName()" (e.g. "item.emerald"), so it would
    ruin the config every time it saved.
    
    Unfortunately, there doesn't seem to be a better way to get registry
    names than by querying the registry.

--- src/main/java/com/guigs44/farmingforengineers/registry/MarketRegistry.java
+++ src/main/java/com/guigs44/farmingforengineers/registry/MarketRegistry.java
@@ -169,7 +169,7 @@ public class MarketRegistry extends AbstractRegistry {
                         String.format(
                                 "%d*%s:%d",
                                 defaultPayment.stackSize,
-                                defaultPayment.getItem().getUnlocalizedName(),
+                                getNamespacedId(defaultPayment),
                                 defaultPayment.getItemDamage()));
             }
             ItemStack itemStack = !value.isEmpty() ? parseItemStack(value) : null;
